dbf1d40afbe621dd37f3f1412414612e8480009b
nikitarredline <52927806+nikitarredline@users.noreply.github.com>
2020-12-30 03:52:24 +0300

    Initial commit

---
+++ src/test/java/ru/stqa/pft/rest/tests/RestTests.java
@@ -0,0 +1,29 @@
+package ru.stqa.pft.rest.tests;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
+import org.apache.http.client.fluent.Executor;
+import org.apache.http.client.fluent.Request;
+import org.apache.http.message.BasicNameValuePair;
+import org.testng.annotations.Test;
+import ru.stqa.pft.rest.model.Issue;
+
+import java.io.IOException;
+import java.util.Set;
+
+import static org.testng.Assert.assertEquals;
+
+public class RestTests extends TestBase {
+
+    @Test
+    public void testCreateIssue() throws IOException {
+        Set<Issue> oldIssues = app.rest().getIssues();
+        Issue newIssue = new Issue().withSubject("Test issue").withDescription("New test issue");
+        int issueId = app.rest().createIssue(newIssue);
+        Set<Issue> newIssues = app.rest().getIssues();
+        oldIssues.add(newIssue.withId(issueId));
+        assertEquals(newIssues, oldIssues);
+    }
+}
